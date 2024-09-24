d1765201063039092650f7a29de7fd21acb968e0
Victot <victorfarian@gmail.com>
2024-09-24 16:13:41 -0300

    TilePane

--- exercicio-javafx/src/layout/Quadrado.java
+++ exercicio-javafx/src/layout/Quadrado.java
@@ -12,13 +12,13 @@ private static int i = 0;
 	};
 	
 	public Quadrado() {
-		this(100, 100);
+		this(100);
 	}
 	
-	public Quadrado(int largura, int altura) {
+	public Quadrado(int tamanho) {
 
-		setWidth(altura);
-		setHeight(altura);
+		setWidth(tamanho);
+		setHeight(tamanho);
 		
 		setFill(Color.web(cores[i]));
 		i++;
--- exercicio-javafx/src/module-info.java
+++ exercicio-javafx/src/module-info.java
@@ -1,5 +1,6 @@
 module exercicosfx {
 	requires javafx.controls;
+	requires javafx.graphics;
 	
 	opens basico;
 	opens layout;
