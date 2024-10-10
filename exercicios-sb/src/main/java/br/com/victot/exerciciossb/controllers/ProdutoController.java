47332ed59c2e8f4e85fac225c5cddcc0c9ce22b2
Victot <victorfarian@gmail.com>
2024-10-10 10:54:55 -0300

    finalizando springboot

--- exercicios-sb/src/main/java/br/com/victot/exerciciossb/controllers/ProdutoController.java
+++ exercicios-sb/src/main/java/br/com/victot/exerciciossb/controllers/ProdutoController.java
@@ -4,6 +4,8 @@ import br.com.victot.exerciciossb.model.entitys.Produto;
 import br.com.victot.exerciciossb.model.repositories.ProdutoRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
@@ -16,7 +18,7 @@ public class ProdutoController {
     @Autowired
     private ProdutoRepository produtoRepository;
 
-    @PostMapping
+    @RequestMapping(method = {RequestMethod.PUT, RequestMethod.POST})
     public @ResponseBody Produto novoProduto(@Valid Produto produto) {
         produtoRepository.save(produto);
         return produto;
@@ -27,9 +29,37 @@ public class ProdutoController {
         return produtoRepository.findAll();
     }
 
+    @GetMapping(path = "/pagina/{numeroPagina}/{qtdePagina}")
+    public Iterable<Produto> obterProdutosPorPagina(@PathVariable int numeroPagina, @PathVariable int qtdePagina) {
+        if(qtdePagina >= 5) qtdePagina = 5;
+        Pageable page = PageRequest.of(numeroPagina, qtdePagina);
+        return produtoRepository.findAll(page);
+    }
+
     @GetMapping(path = "/{id}")
     public Optional<Produto> obterProdutoPorId(@PathVariable int id) {
         return produtoRepository.findById(id);
     }
+
+    @DeleteMapping(path = "/{id}")
+    public void deletarProdutoPorId(@PathVariable int id) {
+        produtoRepository.deleteById(id);
+    }
+
+    @GetMapping(path = "/nome/{nome}")
+    public Iterable<Produto> obterProdutosPorNome(@PathVariable String nome) {
+        return produtoRepository.findByNomeContaining(nome);
+    }
+
+//    @PostMapping
+//    public @ResponseBody Produto novoProduto(@Valid Produto produto) {
+//        produtoRepository.save(produto);
+//        return produto;
+//    }
+
+//    @PutMapping
+//    public Produto alterarProduto(@Valid Produto produto) {
+//        return produtoRepository.save(produto);
+//    }
 
 }
--- exercicios-sb/src/main/java/br/com/victot/exerciciossb/model/repositories/ProdutoRepository.java
+++ exercicios-sb/src/main/java/br/com/victot/exerciciossb/model/repositories/ProdutoRepository.java
@@ -2,8 +2,9 @@ package br.com.victot.exerciciossb.model.repositories;
 
 import br.com.victot.exerciciossb.model.entitys.Produto;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-public interface ProdutoRepository extends CrudRepository<Produto, Integer> {
+public interface ProdutoRepository extends CrudRepository<Produto, Integer>, PagingAndSortingRepository<Produto, Integer> {
 
-
+    Iterable<Produto> findByNomeContaining(String nome);
 }
